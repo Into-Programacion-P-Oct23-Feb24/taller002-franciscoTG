e2c7e305e929c12c86e5f6c682a4398e68040812
Francisco Torres <ft11056575@gmail.com>
2023-10-19 16:53:08 -0500

    segundo commit

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -14,7 +14,10 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ejemplo de Java\n" +
+"Manejado desde GitHub\n" +
+"Versionado a través de Git\n" +
+"Creado por <franciscoTG>");
     }
     
 }
